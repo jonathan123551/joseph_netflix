d8361359d2816ba1f1c8c72f585b85412228eadd
Jonathan <jonathanmaged80@gmail.com>
2026-06-01 03:30:37 +0300

    fix: add ThrottlerModule to AppModule

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ThrottlerModule } from '@nestjs/throttler';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
@@ -6,7 +7,14 @@ import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [AuthModule, PrismaModule],
+  imports: [
+    AuthModule, 
+    PrismaModule,
+    ThrottlerModule.forRoot([{
+      ttl: 60000,
+      limit: 10,
+    }]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
